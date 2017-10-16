a9a1f8ea4282357e154276a4f6c6cf82570be972
alexeyOmelchenko <ak.omelchenko@gmail.com>
2017-10-16 16:34:33 +0300

    Add files via upload

---
+++ Lesson2/2-operators.js
@@ -0,0 +1,36 @@
+//1
+
+// var r = '9' + 0; // "90"
+// var r = 9 + '0'; // "90"
+// var r = 4 + 9; // 13
+// var r = null + 7; // 7
+// var r = '6' + null; // "6null"
+// var r = '6' + []; // "6"
+// var r = '6' + null; // "6null"
+// console.log(r);
+// console.log(typeof(r));
+
+//2
+
+// 2/3 + 1 + 1/3 == 2
+// False
+// Because 2/3 + 1 + 1/3 will be equal to 1.9999999999999998 not to 2
+
+//3
+
+// var r = 6 && 0 && 7; //  0
+// var r = -9 && -8; // -8
+// var r = 6 && 0 && 7; // 0
+// var r = "string" && 0; // 0
+// var r = [] && {} && 7; // 7
+// var r = [] || 7; // []
+// var r = {} || 0; // {}
+// var r = false || true; // true
+// var r = "2" > "3"; // false
+// var r = "ab" <= "fg"; // true
+// var r = "2k" <= "8l"; // true
+// var r = "2" != 2; // false
+// var r = "2" !== 2; // true
+// var r = ++2 + 2; // 5
+// var r = ++2 + 2--; // 5
+// var r = 1++ - 2--; // -1
