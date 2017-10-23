8b8b23cfdf8e5e0fd375b755dd97c21b69f7eae0
alexeyOmelchenko <ak.omelchenko@gmail.com>
2017-10-23 16:57:15 +0300

    Add files via upload

--- Lesson3/4-functions-simple.js
+++ Lesson3/4-functions-simple.js
@@ -33,24 +33,60 @@
 
 //4
 
-var arr = [];
-var arrs = [];
-
-function getPrimesSumBelow(x) {
-
-// for(var i = 1; i<=x; i++){
-//     arr.push(i);
-// }
-// for(var j = 2; j<=arr.length; j++){
-//          for(var p = ){
-//
-//          }
+// var sum = 0;
+// function getPrimesSumBelow(x){
+//     for (var i=2; i < x; i++){
+//         for (var j=2; j <= i; j++){
+//             if (i === j) {
+//                 sum = sum + i;
+//                 console.log("Prosote " + i);
+//             }
+//             else if(i % j === 0){
+//                console.log("Ne prostoe " + i);
+//                 break;
+//             }
+//         }
 //     }
-//
-// console.log(arrs);
-// console.log(arr);
-    
+//     console.log("Summa prostih = " + sum);
+// }
+// getPrimesSumBelow(10);
 
-}
+//5
 
-getPrimesSumBelow(10); // 2 + 3 + 5 + 7 = 17
+// var x = prompt("Enter nuber");
+// function odd(x){
+//     if(x%2 !== 0 ){
+//         return true;
+//     } else{
+//         return false;
+//     }
+// }
+// odd(x);
+
+//6
+
+// function oddNumbers(x,y){
+// for(x; x<=y; x++){
+//     function odd(x){
+//     if(x%2 !== 0 ){
+//         console.log(x);
+//     } else{
+//         return false;
+//     }
+// }
+// odd(x);
+// }
+// }
+// oddNumbers(1,11);
+
+//7
+
+// function getNine(x){
+//     var x = +prompt("Enter x");
+//     if(x !== 9){
+//         getNine(x);
+//     } else if (x == 9){
+//         console.log("Congrats ");
+//         }
+// }
+// getNine();
---
+++ Lesson3/5-loops-statements.js
@@ -0,0 +1,87 @@
+//1
+
+// for (var i = 0; i <= 15; i++){
+//     if(i%2 !== 0){
+//         console.log(i + " is odd");
+//     } else {
+//         console.log(i + " is even")
+//     }
+// }
+
+//2
+
+// for(var i = 1; i <= 100; i++){
+//
+//     if (i%3 === 0 && i%5 === 0){console.log("FizzBuzz")}
+//     else if (i%5 === 0){ console.log("Buzz")}
+//     else if (i%3 === 0){ console.log("Fizz")}
+//     else {console.log(i)}
+// }
+
+//3
+
+// function GCD (x,y){
+//     var x = +prompt("Enter x");
+//     var y = +prompt("Enter y");
+//     var arrx = [];
+//     var arry = [];
+//
+//     for(var i = 1; i<= x; i++){
+//         if(x%i == 0){
+//             arrx.push(i);
+//             }
+//         else {}
+//     }
+//
+//     for (var j = 1; j<=y; j++){
+//         if(y%j == 0){
+//             arry.push(j);
+//         }
+//     }
+//
+//     var maksElement = 0;
+//     for(var k = 1; k <= arrx.length; k++){
+//         for(var t = 1; t < arry.length; t++){
+//             if(arrx[k] === arry[t]){
+//                 maksElement = arrx[k];
+//             }
+//         }
+//     }
+//     console.log(arrx);
+//     console.log(arry);
+//     console.log(maksElement);
+// }
+// GCD();
+
+//4
+
+// var sum = 0;
+// for(var i = 1; i <= 1000; i++){
+//
+//     if (i%5 === 0 && i%3 ===0){
+//         sum = sum + i;}
+// }
+// console.log(sum);
+
+//5
+
+// for(var i = 10; i <= 90; i++){
+//     if (i%5 === 0 && i%3 === 0){
+//         console.log(i);
+//     }
+// }
+
+//6
+
+// for(var i = 70; i >= 11; i--){
+//     if(i%2 !== 0){
+//         console.log(i);}
+// }
+
+//7
+
+// for(var i = 78; i>=13; i--){
+//     if(i%2 === 0 && i%3 === 0){
+//         console.log(i);
+//     }
+// }
