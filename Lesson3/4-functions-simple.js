5a9b47a12af42bb1f27be20e2382678ebbc82d9a
Karpenko Lily <kl2.karpenko@gmail.com>
2017-10-25 23:25:48 +0300

    Update 4-functions-simple.js

--- Lesson3/4-functions-simple.js
+++ Lesson3/4-functions-simple.js
@@ -20,10 +20,16 @@
 // var sum = 0;
 //
 // function range(x, y) {
-//
+//   // leave first statement empty if there is nothing to create
+      // for(; x <= y; x++) 
+
+    // why you add it first to array and only after that you sum it? it's not needed here
+
 //     for(x; x <= y; x++){
 //         arr.push(x);
 //     }
+
+
 //     for (var i = 0; i < arr.length; i++){
 //         sum = sum + arr[i];
 //     }
@@ -60,6 +66,8 @@
 //     } else{
 //         return false;
 //     }
+
+// or you can just return x%2 !== 0; from function
 // }
 // odd(x);
 
@@ -67,6 +75,7 @@
 
 // function oddNumbers(x,y){
 // for(x; x<=y; x++){
+// you have function odd declaired on the top you should use it and not create a new one on each for iteration
 //     function odd(x){
 //     if(x%2 !== 0 ){
 //         console.log(x);
